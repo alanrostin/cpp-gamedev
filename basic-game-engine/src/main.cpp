f50bc9e45f60ffa84589915f2af569327e1326ac
alanrostin <alan.rostin@gmail.com>
2023-12-21 19:31:24 +0100

    add SDL2 test window

--- basic-game-engine/src/main.cpp
+++ basic-game-engine/src/main.cpp
@@ -1,8 +1,29 @@
-#include <iostream>
+#include <SDL2/SDL.h>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Basic Game Engine" << std::endl;
+    // Screen dimension constants
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 450;
+
+    // Window pointer
+    SDL_Window* window = nullptr;
+
+    // Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Create window
+    window = SDL_CreateWindow("First Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+                                SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    
+    // Wait two seconds
+    SDL_Delay(2000);
+
+    // Destroy window
+    SDL_DestroyWindow(window);
+
+    // Quit SDL
+    SDL_Quit();
 
     return 0;
 }
